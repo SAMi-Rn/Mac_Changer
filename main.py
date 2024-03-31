4ae873a4e6afd58ba403e0cc93f19c21d9b50da4
SAMi-Rn <sami_roudgarian@yahoo.com>
2024-03-31 00:53:00 -0700

    parse command line arguments

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import subprocess
+import optparse
 
-print("[+] ifconfig en0 before changing MAC address")
-subprocess.call(["ifconfig", "en0", "down"])
-subprocess.call(["sudo", "ifconfig", "en0", "hw", "ether", "00:11:22:33:44:55"])
-subprocess.call(["ifconfig", "en0", "up"])
-print("[+] ifconfig en0 after changing MAC address")
+parser = optparse.OptionParser()
+parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+(options, arguments) = parser.parse_args()
+
+interface = options.interface
+new_mac = options.new_mac
+
+print("[+] Changing MAC address for " + interface + " to " + new_mac)
+
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+subprocess.call(["ifconfig", interface, "up"])
+
 
 
 
